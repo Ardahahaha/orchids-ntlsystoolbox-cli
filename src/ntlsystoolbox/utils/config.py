dea91c591e5eb7eb0f8292c94e771ca6f70aaadb
arda <ardasongur561@gmail.com>
2025-12-18 11:21:15 +0100

    Auto-sync after agent response

--- src/ntlsystoolbox/utils/config.py
+++ src/ntlsystoolbox/utils/config.py
@@ -35,12 +35,20 @@ def load_config() -> Dict[str, Any]:
     config["mysql"]["database"] = os.getenv("MYSQL_DATABASE", config.get("mysql", {}).get("database", "wms"))
     
     config.setdefault("ad", {})
-    config["ad"]["domain_controllers"] = os.getenv("AD_DOMAIN_CONTROLLERS", 
-                                                    config.get("ad", {}).get("domain_controllers", "")).split(",")
+    ad_dc_env = os.getenv("AD_DOMAIN_CONTROLLERS")
+    if ad_dc_env:
+        config["ad"]["domain_controllers"] = ad_dc_env.split(",")
+    else:
+        dc_value = config.get("ad", {}).get("domain_controllers", [])
+        config["ad"]["domain_controllers"] = dc_value if isinstance(dc_value, list) else dc_value.split(",") if dc_value else []
     
     config.setdefault("dns", {})
-    config["dns"]["servers"] = os.getenv("DNS_SERVERS", 
-                                         config.get("dns", {}).get("servers", "")).split(",")
+    dns_env = os.getenv("DNS_SERVERS")
+    if dns_env:
+        config["dns"]["servers"] = dns_env.split(",")
+    else:
+        dns_value = config.get("dns", {}).get("servers", [])
+        config["dns"]["servers"] = dns_value if isinstance(dns_value, list) else dns_value.split(",") if dns_value else []
     
     config.setdefault("backup", {})
     config["backup"]["path"] = os.getenv("BACKUP_PATH", config.get("backup", {}).get("path", "./backups"))
